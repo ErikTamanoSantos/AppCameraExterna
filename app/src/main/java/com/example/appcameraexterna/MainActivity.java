c785ed7b74c16f370e071c414d1b43bdd8ee03b9
eriktamanosantos <eriktamano@gmail.com>
2023-01-24 16:39:20 +0100

    Fer fotos (thumbnail)

--- app/src/main/java/com/example/appcameraexterna/MainActivity.java
+++ app/src/main/java/com/example/appcameraexterna/MainActivity.java
@@ -5,25 +5,37 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
-    ActivityResultLauncher<Intent> someActivityResultLauncher;
+    ActivityResultLauncher<Intent> galleryActivityResultLauncher;
+    ActivityResultLauncher<Intent> cameraActivityResultLauncher;
+    public Uri photoURI;
+    String currentPhotoPath;
     public static int RC_PHOTO_PICKER = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button boton = findViewById(R.id.boton);
-        someActivityResultLauncher = registerForActivityResult(
+        Button boton_cargar = findViewById(R.id.boton_cargar);
+        Button boton_foto = findViewById(R.id.boton_foto);
+        galleryActivityResultLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 new ActivityResultCallback<ActivityResult>() {
                     @Override
@@ -38,13 +50,35 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
 
-        boton.setOnClickListener(new View.OnClickListener() {
+        cameraActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            //Intent data = result.getData();
+                            //Bundle extras = data.getExtras();
+                            // imageBitmap = (Bitmap) extras.get("data");
+                            ImageView imageView = findViewById(R.id.img);
+                            //imageView.setImageBitmap(imageBitmap);
+                            imageView.setImageURI(photoURI);
+                        }
+
+                    }
+                });
+        boton_cargar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 openSomeActivityForResult(view);
             }
         });
 
+        boton_foto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
 
     public void openSomeActivityForResult(View view) {
@@ -54,6 +88,42 @@ public class MainActivity extends AppCompatActivity {
         intent.setType("image/jpg");
         intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
         //Launch activity to get result
-        someActivityResultLauncher.launch(intent);
+        galleryActivityResultLauncher.launch(intent);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            photoURI = FileProvider.getUriForFile(this,
+                    "com.example.appcameraexterna.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+        }
+        cameraActivityResultLauncher.launch(takePictureIntent);
     }
 }
